3fb0b6c243a3f5e20965c02f26cbae3e8cbb4473
José Garção <josemariagarcao15@gmail.com>
2022-11-22 13:03:39 +0000

    init express tutorial

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,8 @@
-console.log('Express Tutorial')
+const http = require("http");
+
+const server = http.createServer((req, resp) => {
+    console.log("User hit the server");
+    resp.end("Home page");
+});
+
+server.listen(5000);
