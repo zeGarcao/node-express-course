e461f94bafed1446c60d8add7e8516c2712c0d2b
José Garção <josemariagarcao15@gmail.com>
2022-11-26 19:54:11 +0000

    express query string

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,21 +1,52 @@
 const express = require("express");
-const path = require("path");
-
 const app = express();
+const { products } = require("./data");
 
-// setup static and middleware
-app.use(express.static("./public"));
+app.get("/", (req, res) => {
+    res.send("<h1>Home Page</h1>" + "<a href='/api/products'>products</a>");
+});
 
-// app.get("/", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
-//     1. adding to static assets
-//     2. SSR - Server Side Rendering
-// });
+app.get("/api/products", (req, res) => {
+    const newProducts = products.map(product => {
+        const { id, name, image } = product;
+        return { id, name, image };
+    });
+    res.json(newProducts);
+});
 
-app.all("*", (req, res) => {
-    res.status(404).send("resource not found");
+app.get("/api/products/:productID", (req, res) => {
+    const { productID } = req.params;
+    const singleProduct = products.find(
+        product => product.id === Number(productID)
+    );
+    if (!singleProduct) {
+        return res.status(404).send("Product Does Not Exist");
+    }
+    return res.json(singleProduct);
+});
+
+app.get("/api/v1/query", (req, res) => {
+    const { search, limit } = req.query;
+    let sortedProducts = [...products];
+    console.log(search);
+    if (search) {
+        sortedProducts = sortedProducts.filter(product => {
+            return product.name.startsWith(search);
+        });
+    }
+    if (limit) {
+        sortedProducts = sortedProducts.slice(0, Number(limit));
+    }
+    if (sortedProducts.length < 1) {
+        // return res.status(200).send("No products matched your search");
+        return res.status(200).json({
+            success: true,
+            data: [],
+        });
+    }
+    return res.status(200).json(sortedProducts);
 });
 
 app.listen(5000, () => {
-    console.log("Server is listening on port 5000....");
+    console.log("Server is listening on port 5000");
 });
