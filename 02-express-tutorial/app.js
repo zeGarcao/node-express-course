16074aced2fa4cef1e05fc11336834e295bf0f9e
José Garção <josemariagarcao15@gmail.com>
2022-11-27 14:11:16 +0000

    express middleware basic

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,50 +1,21 @@
 const express = require("express");
 const app = express();
-const { products } = require("./data");
 
-app.get("/", (req, res) => {
-    res.send("<h1>Home Page</h1>" + "<a href='/api/products'>products</a>");
+// req => middleware => res
+const logger = (req, res, next) => {
+    const method = req.method;
+    const url = req.url;
+    const date = new Date().getFullYear();
+    console.log(method, url, date);
+    next();
+};
+
+app.get("/", logger, (req, res) => {
+    res.send("Home Page");
 });
 
-app.get("/api/products", (req, res) => {
-    const newProducts = products.map(product => {
-        const { id, name, image } = product;
-        return { id, name, image };
-    });
-    res.json(newProducts);
-});
-
-app.get("/api/products/:productID", (req, res) => {
-    const { productID } = req.params;
-    const singleProduct = products.find(
-        product => product.id === Number(productID)
-    );
-    if (!singleProduct) {
-        return res.status(404).send("Product Does Not Exist");
-    }
-    return res.json(singleProduct);
-});
-
-app.get("/api/v1/query", (req, res) => {
-    const { search, limit } = req.query;
-    let sortedProducts = [...products];
-    console.log(search);
-    if (search) {
-        sortedProducts = sortedProducts.filter(product => {
-            return product.name.startsWith(search);
-        });
-    }
-    if (limit) {
-        sortedProducts = sortedProducts.slice(0, Number(limit));
-    }
-    if (sortedProducts.length < 1) {
-        // return res.status(200).send("No products matched your search");
-        return res.status(200).json({
-            success: true,
-            data: [],
-        });
-    }
-    return res.status(200).json(sortedProducts);
+app.get("/about", logger, (req, res) => {
+    res.send("About Page");
 });
 
 app.listen(5000, () => {
